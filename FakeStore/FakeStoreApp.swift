6e7eac10b902e9ff123afa921cc517ad70d20f67
Dawood Akbar <dawood_akbar@ml-bench.com>
2026-01-18 18:29:11 +0500

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Dawood on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
